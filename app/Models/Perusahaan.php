acda068006d5320f3717364862fd669af98a4ba2
kimp <ahmadmiqdad27@gmail.com>
2024-08-23 00:48:42 +0700

    upload

---
+++ app/Models/Perusahaan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Perusahaan extends Model
+{
+    protected $table = 'perusahaan';
+    protected $fillable = ['nama_perusahaan','lokasi','posisi'];
+}
---
+++ resources/views/components/job-table.blade.php
@@ -0,0 +1,25 @@
+<div class="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
+    <table class="min-w-full bg-white border border-gray-200">
+        <thead>
+        <tr class="bg-gray-100">
+            <td class="border w-1/4 py-4 px-6 text-left text-gray-600 font-bold">No.</td>
+            <td class="border w-1/4 py-4 px-6 text-left text-gray-600 font-bold">Nama Perusahaan</td>
+            <td class="border w-1/4 py-4 px-6 text-left text-gray-600 font-bold">Lokasi</td>
+            <td class="border w-1/4 py-4 px-6 text-left text-gray-600 font-bold">Posisi</td>
+            <td class="border w-1/4 py-4 px-6 text-left text-gray-600 font-bold">Status</td>
+        </tr>
+        </thead>
+        <tbody class="bg-white">
+        @php $no = 1; @endphp
+        @foreach($list as $items)
+            <tr>
+                <td class="border py-4 px-6 border-b border-gray-200">{{ $no++ }}</td>
+                <td class="border py-4 px-6 border-b border-gray-200">{{ $items->perusahaan }}</td>
+                <td class="border py-4 px-6 border-b border-gray-200">{{ $items->lokasi }}</td>
+                <td class="border py-4 px-6 border-b border-gray-200">{{ $items->posisi }}</td>
+                <td class="border py-4 px-6 border-b border-gray-200"><span class="bg-red-500 text-white py-1 px-2 rounded-full text-xs">{{ $items->status }}</span></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
