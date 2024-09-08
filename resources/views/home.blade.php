53630068f1fe4570c0e87f5bdb63586fd33fd879
Miqdad <ahmadkimpir@gmail.com>
2024-09-08 20:41:54 +0700

    Add Search Box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
             <a href="{{ route('perusahaan.create') }}" class="flex justify-end focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-cyan-900 mb-3">Tambah</a>
         </div>
         <div class="container">
-            <input type="text" name="search" id="search" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600" placeholder="Search..." x-model="searchList">
+            <input type="text" name="search" id="search" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-amber-100 dark:text-blue-800 dark:bg-gray-600 mb-3" placeholder="Search..." x-model="searchList">
         </div>
         <x-job-table :list="$list"/>
     </div>
