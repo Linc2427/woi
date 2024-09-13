6b53462b1fc01cb4ae34267644c2de8a3f31ba7b
Miqdad <ahmadkimpir@gmail.com>
2024-09-14 05:29:00 +0700

    fix action table column

--- resources/views/components/job-table.blade.php
+++ resources/views/components/job-table.blade.php
@@ -27,21 +27,24 @@
                         <span class="{{ $items->status === 'Applied' ? 'bg-indigo-600 dark:bg-indigo-500 text-white' : ($items->status === 'Test' ? 'bg-lime-500 dark:bg-lime-400 text-white' : ($items->status === 'Accepted' ? 'bg-green-500 dark:bg-green-400 text-white' : 'bg-red-500 dark:bg-red-400 text-white')) }} py-1 px-2 rounded-full text-xs font-bold">{{ $items->status }}</span>
                     </td>
                     <td class="border py-4 px-6 text-gray-600 dark:text-gray-300">{{ \Carbon\Carbon::parse($items['created_at'])->format('d M Y') }}</td>
-                    <td class="border py-4 px-6 flex justify-center">
-                        <a href="/job-detail/{{ $items->id }}" class="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:bg-violet-800 dark:hover:bg-violet-900 dark:text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Detail</a>
-                        <a href="{{ route('perusahaan.edit', $items->id) }}" class="focus:outline-none text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Edit</a>
-                        <form action="{{ route('perusahaan.destroy', $items->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this item?');">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="focus:outline-none text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Delete</button>
-                        </form>
+                    <td class="border py-4 px-6 text-center">
+                        <div class="flex justify-center space-x-2">
+                            <a href="/job-detail/{{ $items->id }}" class="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:bg-violet-800 dark:hover:bg-violet-900 dark:text-white font-medium rounded-lg text-sm px-5 py-2.5">Detail</a>
+                            <a href="{{ route('perusahaan.edit', $items->id) }}" class="focus:outline-none text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5">Edit</a>
+                            <form action="{{ route('perusahaan.destroy', $items->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this item?');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="focus:outline-none text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5">Delete</button>
+                            </form>
+                        </div>
                     </td>
+
                 </tr>
             @endforeach
             </tbody>
         </table>
+    </div>
         <div class="mt-3 mb-3 px-2 text-gray-600 dark:text-gray-300">
             {{ $list->links() }}
         </div>
-    </div>
 </div>
